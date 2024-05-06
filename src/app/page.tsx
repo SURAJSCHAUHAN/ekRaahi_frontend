e4fa13fede2b11018715f7f9865873030a3544a4
SURAJSCHAUHAN <chauhansurajsingh.2020cs@technonjr.org>
2024-05-06 22:00:57 +0530

    swiper removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import BestSellers from '@/components/BestSellers'
-import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
-import Navbar from '@/components/Navbar'
 import NewArrivals from '@/components/NewArrivals'
-import NewEra from '@/components/NewEra'
-import Recommended from '@/components/Recommended'
 import ServicesIcon from '@/components/ServicesIcon'
 import SideMenu from '@/components/SideMenu'
 import TopCategories from '@/components/TopCategories'
@@ -19,9 +15,7 @@ const page = () => {
       <TopCategories/>
       <NewArrivals/>
       <ServicesIcon/>
-      <NewEra/>
       <BestSellers/>
-      {/* <Recommended/> */}
     </div>
   )
 }
